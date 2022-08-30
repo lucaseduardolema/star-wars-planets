fff5d14c84c25d0dd5000e6acb76c86479a73cd9
lucaseduardolema <lucaseduardolema@gmail.com>
2022-08-30 20:56:48 -0300

    refact fetch planets

--- src/services/fetchPlanets.js
+++ src/services/fetchPlanets.js
@@ -1,8 +1,12 @@
 const fetchPlanets = async () => {
-  const url = "https://swapi-trybe.herokuapp.com/api/planets/";
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const url = "https://swapi-trybe.herokuapp.com/api/planets/";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default fetchPlanets;
